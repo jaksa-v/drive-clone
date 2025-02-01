9be0455cbeb7fcd350bca00c1b4844b4e3e4a43b
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-02-01 10:52:01 +0100

    DB model working!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,14 +2,40 @@ import {
   int,
   text,
   singlestoreTableCreator,
+  index,
+  bigint,
 } from "drizzle-orm/singlestore-core";
 
 export const createTable = singlestoreTableCreator(
   (name) => `drive_clone_${name}`,
 );
 
-export const users = createTable("users", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const files = createTable(
+  "files_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = createTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
